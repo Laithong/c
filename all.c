d36ac87bf3d554b88ac11a8559048380d969f5c7
Laithong <40286402+Laithong@users.noreply.github.com>
2018-08-23 02:52:58 +0700

    Create all.c

---
+++ all.c
@@ -0,0 +1,265 @@
+
+
+                  //              c++ programming
+
+
+
+
+#include <stdio.h>
+#include <conio.h>
+main(){
+	int a,b,c;
+	a=45;
+	b=56;
+	c=a+b-1;
+		printf ("%d\n",c);
+		
+		
+		
+		
+		
+/*		
+	
+main (){
+	
+	int x,y,z ;
+	printf ("enter number x=");
+	scanf ("%d",&x);
+	    printf ("enter number y=");
+		scanf ("%d",&y);
+	printf ("x + y = %d\n", x+y);
+	printf ("enter number z=");
+	scanf ("%d",&z);
+    printf ("(x+y) / z = %d\n", (x+y)/z);
+    
+    */
+    
+    
+    
+    
+    
+   /* main (){
+	int a,b,c,d;
+	a= (8+2)+74/2;
+	b= 4+6/2;
+	c= (4+6)/2;
+	d= 8+52+75;
+	printf("\n\n\n");
+	printf("(8+2)+74/2=%d\n",a);
+	printf("4+6/2=%d\n",b);
+	printf("(4+6)/2=%d\n",c);
+	printf("8+52+75=%d\n",d);*/
+	
+	
+	
+	
+	
+	
+	
+/*main(){
+
+int num1=2, num2=4;
+    printf ("1=3=%d\n", num1>num2);
+    printf ("1!=3=%d\n", num1!=num2);
+    printf ("1<=3=%d\n", num1<=num2);
+	printf ("1>=3=%d\n", num1>=num2);
+	printf ("1>3=%d\n", num1>num2);
+	printf ("1<3=%d\n", num1<num2);
+	*/
+	
+	
+	
+	
+	
+	
+	
+/*	
+	
+	main(){
+	int p;
+		printf("\n\t\t\t\tGrade you ");
+	
+      	printf("\ninpout score= ");
+      	
+      	scanf("%d",&p);
+      	
+    	if ( p<=100 && p>=90){
+    		printf("\nGrade a");
+    		      }
+					  
+		else if (p<=89) {
+				printf("\nGrade b ");
+			        	}
+			        	
+		else if (10<=p>0) {
+			     	printf("\nGrade c ");
+			           }
+			           	else if (p=0) {
+			     	printf("\nGrade f ");
+			           }
+		
+		else {
+				printf("Error");
+			}
+	
+	*/
+	
+	
+	
+	
+	
+	
+	
+	
+	/*	int main() {
+    int i;
+
+    for (i = 10; i >= 1; i--) {
+        printf ("%d\n",i); 
+    }*/
+    
+    
+    
+    
+    
+    
+    
+   /*	int main() {
+    int i;
+
+    for ( i=2; i<= 10; i=i+2) {
+        printf ("%d\n",i); 
+    }*/
+    
+    
+    
+    
+    
+    /*	int main() {
+    int i=1;
+    while  ( i<= 10) {		
+		printf("%d\n",i);
+		i++; 
+    }*/
+    
+    
+    
+    
+    /*	int main() {
+    int i=1;
+
+   do {
+   	
+   	printf ("%d\n",i);
+   	i++;
+   }	
+   while ( i<=10 );*/
+   
+   
+   
+   
+   
+   
+   
+/*
+ 
+int main() {
+  
+    int i, j, n;
+  
+    for(i=1; i<=12; i++){
+        n = 2;
+        for(j=1; j<=6; j++){
+            printf("%5dx%2d=%3d", n, i, n*i);
+            n++;
+        }
+        printf("\n");
+    }
+  
+    printf("\n\n");
+  
+    for(i=1; i<=12; i++){
+        n = 8;
+        for(j=1; j<=6; j++){
+            printf("%5dx%2d=%3d", n, i, n*i);
+            n++;
+        }
+        printf("\n");
+    }
+  
+    printf("\n");  */ 
+
+
+
+/*
+
+int main()
+{
+	char name[12][10]={"Beer","Spy","wine","Pepsi","cooktel"};
+	for ( int i=0; i<5;i++){
+		printf ("%s\n",name[i]);
+	
+*/
+
+
+
+/*
+int main ()
+{
+    int m[3][4] = {
+    {1, 2,  3,  4},
+    {5, 6,  7,  8},
+    {9, 10, 11, 12}};
+    for (int i=0; i<3; i++){
+    	for (int j=0; j<4; j++){
+    		 printf("%d\n",m[i][j]); 
+		} 		
+	}*/
+
+	
+
+
+  
+   
+    
+  /*  
+    
+     
+int main()
+{
+	char name[]={'a','s','d','u','i'};
+	for ( i=0; i<5;i++){
+		printf ("%c\n",name[i]);
+	}
+    
+    
+    */
+    
+    
+    
+ /*   
+
+int main()
+{
+
+	
+	
+	char *m[]={"January","February","March","April","April","June",
+	            "July","August","September","October","November","December"};
+	 int i;
+     scanf("%d",&i);
+		printf ("%s\n",m[i-1]);
+	
+
+
+
+
+*/
+getch();
+}
+
+    
+    
+
+
+
